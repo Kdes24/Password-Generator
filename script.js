a1735ee4738731c22b259e2f1a479b773f6eda7e
Kevin <kmdesbiens@gmail.com>
2023-04-06 14:37:15 -0400

    commit

--- script.js
+++ script.js
@@ -15,3 +15,54 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  // Prompt the user for password criteria (e.g. length, types of characters)
+  var passwordLength = parseInt(prompt("How many characters would you like your password to be? (Choose between 8 and 128)"));
+
+  // Check if the password length is within the allowed range
+  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+    alert("Please enter a valid number between 8 and 128.");
+    return "";
+  }
+
+  var includeLowercase = confirm("Would you like to include lowercase letters?");
+  var includeUppercase = confirm("Would you like to include uppercase letters?");
+  var includeNumbers = confirm("Would you like to include numbers?");
+  var includeSymbols = confirm("Would you like to include symbols?");
+
+  // Check if at least one type of character is selected
+  if (!includeLowercase && !includeUppercase && !includeNumbers && !includeSymbols) {
+    alert("Please select at least one type of character.");
+    return "";
+  }
+
+  // Define character sets based on selected criteria
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numberChars = "0123456789";
+  var symbolChars = "!@#$%^&*()_+-={}[]|\\:;\"'<>,.?/";
+
+  var availableChars = "";
+  if (includeLowercase) {
+    availableChars += lowercaseChars;
+  }
+  if (includeUppercase) {
+    availableChars += uppercaseChars;
+  }
+  if (includeNumbers) {
+    availableChars += numberChars;
+  }
+  if (includeSymbols) {
+    availableChars += symbolChars;
+  }
+
+  // Generate the password using the selected characters
+  var password = "";
+  for (var i = 0; i < passwordLength; i++) {
+    var randomIndex = Math.floor(Math.random() * availableChars.length);
+    password += availableChars[randomIndex];
+  }
+
+  return password;
+}
